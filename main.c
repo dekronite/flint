640007b1268d2aa5c4bd506b61b96abf70ea7750
dekronite <200502339+dekronite@users.noreply.github.com>
2026-06-04 18:25:38 +0100

    disable ctrlq,s,v fix ctrl m turn off opost

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-#include <unistd.h>
 #include <ctype.h>
 #include <stdio.h>
-#include <termios.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
 struct termios orig_termios;
+
 void disableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
@@ -12,19 +13,26 @@ void disableRawMode() {
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
+
   struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-    enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-      if (iscntrl(c)) {
-        printf("%d\n", c);
-      } else {
-        printf("%d ('%c')\n", c, c);
-      }
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
     }
+  }
+
+  return 0;
 }
